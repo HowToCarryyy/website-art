8340d933911a2a668caeafeceaa32946e7a884b4
HowToCarryyy <howtobecarried@web.de>
2021-11-13 13:50:16 +0100

    Galerie und Menü

---
+++ js/js_galerie.js
@@ -0,0 +1,15 @@
+const arrow_a = document.querySelector('.arrow.a');
+const desc_aside_a = document.querySelector('.desc-aside.a');
+
+arrow_a.addEventListener('click', function() {
+    this.classList.toggle('is-active');
+    desc_aside_a.classList.toggle('is-active');
+});
+
+const arrow_b = document.querySelector('.arrow.b');
+const desc_aside_b = document.querySelector('.desc-aside.b');
+
+arrow_b.addEventListener('click', function() {
+    this.classList.toggle('is-active');
+    desc_aside_b.classList.toggle('is-active');
+});
