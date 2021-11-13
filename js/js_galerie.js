e722b3557985048dbbdd2c7e3217c2ef07502414
HowToCarryyy <howtobecarried@web.de>
2021-11-13 16:00:14 +0100

    price update

--- js/js_galerie.js
+++ js/js_galerie.js
@@ -1,13 +1,12 @@
 const arrow_a = document.querySelector('.arrow.a');
 const desc_aside_a = document.querySelector('.desc-aside.a');
+const arrow_b = document.querySelector('.arrow.b');
+const desc_aside_b = document.querySelector('.desc-aside.b');
 
 arrow_a.addEventListener('click', function() {
     this.classList.toggle('is-active');
     desc_aside_a.classList.toggle('is-active');
 });
-
-const arrow_b = document.querySelector('.arrow.b');
-const desc_aside_b = document.querySelector('.desc-aside.b');
 
 arrow_b.addEventListener('click', function() {
     this.classList.toggle('is-active');
